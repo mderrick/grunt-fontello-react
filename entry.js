f87835847e2d18d0203217bfd0cd32c6e61acafe
Matt Derrick <git@mattderrick.co.uk>
2015-06-24 20:58:06 +0100

    tabs to spaces

--- entry.js
+++ entry.js
@@ -1,17 +1,17 @@
 var React = require('react'),
     Icon = require('./components/view.js'),
     Content = React.createClass({
-		render: function() {
-			return (
-				<div>
-					<Icon name="stackoverflow"/>
-					<Icon name="github-squared"/>
-				</div>
-			);
-		}
-	});
+        render: function() {
+            return (
+                <div>
+                    <Icon name="stackoverflow"/>
+                    <Icon name="github-squared"/>
+                </div>
+            );
+        }
+    });
 
 React.render(
-	<Content/>,
+    <Content/>,
     document.getElementById('content')
 );
